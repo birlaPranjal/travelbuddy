7e525b58d490c995ec9fcfc79a51d958299f6d54
Pranjal birla <birlapranjal460@gmail.com>
2024-08-27 23:44:15 +0530

    added chat items

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+export default function ChatPage() {
+    return (
+        <div className="chat-page">
+            <div className="chat-container">
+                {/* Render chat messages here */}
+            </div>
+            <div className="input-container">
+                {/* Render input field and send button here */}
+            </div>
+        </div>
+    );
 }
