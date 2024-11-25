be4243340e7bc7bcf308f5a4718cc7b71ac6c06a
Pranjal Birla <birlapranjal460@gmail.com>
2024-11-25 17:50:09 +0530

    added fixes

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,74 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import { compare } from "bcryptjs";
+import UserModel from "../model/User";
+import dbConnect from "./dbConnect";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        await dbConnect();
+        const user = await UserModel.findOne({ email: credentials.email });
+
+        if (!user || !user?.password) {
+          throw new Error('Invalid credentials');
+        }
+
+        const isPasswordValid = await compare(credentials.password, user.password);
+
+        if (!isPasswordValid) {
+          throw new Error('Invalid credentials');
+        }
+
+        return {
+          id: user._id.toString(),
+          email: user.email,
+          name: user.name,
+          isVerified: user.isVerified,
+        };
+      }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_SECRET || "",
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.isVerified = user.isVerified;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.isVerified = token.isVerified;
+      }
+      return session;
+    }
+  }
+}; 
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,32 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    id: string;
+    email: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+    isNewUser?: boolean;
+  }
+
+  interface Session {
+    user: {
+      _id: string;
+      isVerified?: boolean;
+      isAcceptingMessages?: boolean;
+      username?: string;
+      isNewUser?: boolean;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+    isNewUser?: boolean;
+  }
+} 
